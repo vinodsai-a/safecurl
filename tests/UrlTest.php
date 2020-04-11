987d703c0aa464e27f231986971040826455a70d
Jeremy Benoist <jeremy.benoist@gmail.com>
2020-04-11 16:12:39 +0200

    Dropping PHP < 7.2
    Rewrote test to be compliant with PHPUnit 8 & 9

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -3,19 +3,51 @@
 use fin1te\SafeCurl\Options;
 use fin1te\SafeCurl\Url;
 
-class UrlTest extends \PHPUnit_Framework_TestCase
+class UrlTest extends \PHPUnit\Framework\TestCase
 {
     public function dataForValidate()
     {
         return array(
-            array(null, 'fin1te\SafeCurl\Exception\InvalidURLException', 'Provided URL "" cannot be empty'),
-            array('http://user@:80', 'fin1te\SafeCurl\Exception\InvalidURLException', 'Error parsing URL "http://user@:80"'),
-            array('http:///example.com/', 'fin1te\SafeCurl\Exception\InvalidURLException', 'Error parsing URL "http:///example.com/"'),
-            array('http://:80', 'fin1te\SafeCurl\Exception\InvalidURLException', 'Error parsing URL "http://:80"'),
-            array('/nohost', 'fin1te\SafeCurl\Exception\InvalidURLException', 'Provided URL "/nohost" doesn\'t contain a hostname'),
-            array('ftp://domain.io', 'fin1te\SafeCurl\Exception\InvalidURLException\InvalidSchemeException', 'Provided scheme "ftp" doesn\'t match whitelisted values: http, https'),
-            array('http://domain.io:22', 'fin1te\SafeCurl\Exception\InvalidURLException\InvalidPortException', 'Provided port "22" doesn\'t match whitelisted values: 80, 443, 8080'),
-            array('http://login:password@google.fr:80', 'fin1te\SafeCurl\Exception\InvalidURLException', 'Credentials passed in but "sendCredentials" is set to false'),
+            array(
+                null,
+                \fin1te\SafeCurl\Exception\InvalidURLException::class,
+                'Provided URL "" cannot be empty',
+            ),
+            array(
+                'http://user@:80',
+                \fin1te\SafeCurl\Exception\InvalidURLException::class,
+                'Error parsing URL "http://user@:80"',
+            ),
+            array(
+                'http:///example.com/',
+                \fin1te\SafeCurl\Exception\InvalidURLException::class,
+                'Error parsing URL "http:///example.com/"',
+            ),
+            array(
+                'http://:80',
+                \fin1te\SafeCurl\Exception\InvalidURLException::class,
+                'Error parsing URL "http://:80"',
+            ),
+            array(
+                '/nohost',
+                \fin1te\SafeCurl\Exception\InvalidURLException::class,
+                'Provided URL "/nohost" doesn\'t contain a hostname',
+            ),
+            array(
+                'ftp://domain.io',
+                \fin1te\SafeCurl\Exception\InvalidURLException\InvalidSchemeException::class,
+                'Provided scheme "ftp" doesn\'t match whitelisted values: http, https',
+            ),
+            array(
+                'http://domain.io:22',
+                \fin1te\SafeCurl\Exception\InvalidURLException\InvalidPortException::class,
+                'Provided port "22" doesn\'t match whitelisted values: 80, 443, 8080',
+            ),
+            array(
+                'http://login:password@google.fr:80',
+                \fin1te\SafeCurl\Exception\InvalidURLException::class,
+                'Credentials passed in but "sendCredentials" is set to false',
+            ),
         );
     }
 
@@ -24,76 +56,71 @@ class UrlTest extends \PHPUnit_Framework_TestCase
      */
     public function testValidateUrl($url, $exception, $message)
     {
-        $this->setExpectedException($exception, $message);
+        $this->expectException($exception);
+        $this->expectExceptionMessage($message);
 
         Url::validateUrl($url, new Options());
     }
 
-    /**
-     * @expectedException \fin1te\SafeCurl\Exception\InvalidURLException\InvalidSchemeException
-     * @expectedExceptionMessage Provided scheme "http" matches a blacklisted value
-     */
     public function testValidateScheme()
     {
+        $this->expectException(\fin1te\SafeCurl\Exception\InvalidURLException\InvalidSchemeException::class);
+        $this->expectExceptionMessage('Provided scheme "http" matches a blacklisted value');
+
         $options = new Options();
         $options->addToList('blacklist', 'scheme', 'http');
 
         Url::validateUrl('http://www.fin1te.net', $options);
     }
 
-    /**
-     * @expectedException \fin1te\SafeCurl\Exception\InvalidURLException\InvalidPortException
-     * @expectedExceptionMessage Provided port "8080" matches a blacklisted value
-     */
     public function testValidatePort()
     {
+        $this->expectException(\fin1te\SafeCurl\Exception\InvalidURLException\InvalidPortException::class);
+        $this->expectExceptionMessage('Provided port "8080" matches a blacklisted value');
+
         $options = new Options();
         $options->addToList('blacklist', 'port', '8080');
 
         Url::validateUrl('http://www.fin1te.net:8080', $options);
     }
 
-    /**
-     * @expectedException \fin1te\SafeCurl\Exception\InvalidURLException\InvalidDomainException
-     * @expectedExceptionMessage Provided host "www.fin1te.net" matches a blacklisted value
-     */
     public function testValidateHostBlacklist()
     {
+        $this->expectException(\fin1te\SafeCurl\Exception\InvalidURLException\InvalidDomainException::class);
+        $this->expectExceptionMessage('Provided host "www.fin1te.net" matches a blacklisted value');
+
         $options = new Options();
         $options->addToList('blacklist', 'domain', '(.*)\.fin1te\.net');
 
         Url::validateUrl('http://www.fin1te.net', $options);
     }
 
-    /**
-     * @expectedException \fin1te\SafeCurl\Exception\InvalidURLException\InvalidDomainException
-     * @expectedExceptionMessage Provided host "www.google.fr" doesn't match whitelisted values: (.*)\.fin1te\.net
-     */
     public function testValidateHostWhitelist()
     {
+        $this->expectException(\fin1te\SafeCurl\Exception\InvalidURLException\InvalidDomainException::class);
+        $this->expectExceptionMessage('Provided host "www.google.fr" doesn\'t match whitelisted values: (.*)\.fin1te\.net');
+
         $options = new Options();
         $options->addToList('whitelist', 'domain', '(.*)\.fin1te\.net');
 
         Url::validateUrl('http://www.google.fr', $options);
     }
 
-    /**
-     * @expectedException \fin1te\SafeCurl\Exception\InvalidURLException\InvalidDomainException
-     * @expectedExceptionMessage Provided host "www.youpi.boom" doesn't resolve to an IP address
-     */
     public function testValidateHostWithnoip()
     {
+        $this->expectException(\fin1te\SafeCurl\Exception\InvalidURLException\InvalidDomainException::class);
+        $this->expectExceptionMessage('Provided host "www.youpi.boom" doesn\'t resolve to an IP address');
+
         $options = new Options();
 
         Url::validateUrl('http://www.youpi.boom', $options);
     }
 
-    /**
-     * @expectedException \fin1te\SafeCurl\Exception\InvalidURLException\InvalidIPException
-     * @expectedExceptionMessage Provided host "2.2.2.2" resolves to "2.2.2.2", which doesn't match whitelisted values: 1.1.1.1
-     */
     public function testValidateHostWithWhitelistIp()
     {
+        $this->expectException(\fin1te\SafeCurl\Exception\InvalidURLException\InvalidIPException::class);
+        $this->expectExceptionMessage('Provided host "2.2.2.2" resolves to "2.2.2.2", which doesn\'t match whitelisted values: 1.1.1.1');
+
         $options = new Options();
         $options->addToList('whitelist', 'ip', '1.1.1.1');
 
@@ -114,12 +141,11 @@ class UrlTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey(0, $res['ips']);
     }
 
-    /**
-     * @expectedException \fin1te\SafeCurl\Exception\InvalidURLException\InvalidIPException
-     * @expectedExceptionMessage Provided host "1.1.1.1" resolves to "1.1.1.1", which matches a blacklisted value: 1.1.1.1
-     */
     public function testValidateHostWithBlacklistIp()
     {
+        $this->expectException(\fin1te\SafeCurl\Exception\InvalidURLException\InvalidIPException::class);
+        $this->expectExceptionMessage('Provided host "1.1.1.1" resolves to "1.1.1.1", which matches a blacklisted value: 1.1.1.1');
+
         $options = new Options();
         $options->addToList('blacklist', 'ip', '1.1.1.1');
 
@@ -138,7 +164,7 @@ class UrlTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('host', $res);
         $this->assertArrayHasKey('ips', $res);
         $this->assertArrayHasKey(0, $res['ips']);
-        $this->assertEquals('http://146.185.175.109:8080', $res['url']);
+        $this->assertEquals('http://104.24.104.11:8080', $res['url']);
         $this->assertEquals('www.fin1te.net', $res['host']);
 
         $res = Url::validateUrl('http://www.fin1te.net:8080', new Options());
